d72e99b9f4c92b6ca029af92d9eb0698ed6dfc50
sihenni <r.s.henni@live.fr>
2023-09-28 12:01:26 +0100

    Ok, done the changes

--- src/com/git/sihenni/MyGitClass2.java
+++ src/com/git/sihenni/MyGitClass2.java
@@ -4,7 +4,7 @@ public class MyGitClass2 {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world");
-		System.out.println("My Changes, I'm Ramzi");
+		System.out.println("My Changes, I'm Ramzi, ok,  I've done changes");
 	}
 
 }
